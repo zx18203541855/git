535b4ae18325586e573cb6c4d3f60551b3b94b52
zx <zx@qq.com>
2022-05-07 12:39:24 +0800

    push commit

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello,Git4");
         System.out.println("hot_fix test");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
