a6da3f6fe49d11114af10d8b41649d7c423ac57c
zx <zx@qq.com>
2022-05-07 11:00:21 +0800

    second commit

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -3,5 +3,6 @@ package com.example.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
+        System.out.println("Hello,Git2");
     }
 }
