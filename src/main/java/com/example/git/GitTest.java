784754703e552a14e36cccdbfe337bf48421812a
zx <zx@qq.com>
2022-05-07 11:10:27 +0800

    hot_fix commit1

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello,Git");
         System.out.println("Hello,Git2");
         System.out.println("Hello,Git3");
+        System.out.println("Hello,Git4");
     }
 }
